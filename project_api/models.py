6dc7b86a31c849b6eb02150647573c8515018bb2
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-24 10:07:55 +0300

    Creating user model

--- project_api/models.py
+++ project_api/models.py
@@ -1,3 +1,10 @@
+from ast import BinOp
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    
+    picture = models.ImageField()
+    user_name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=1500)
+    project= models.FieldFile() 
