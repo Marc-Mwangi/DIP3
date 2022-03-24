3fee4f002ee745311e905eea259393b080019f7a
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-24 10:10:11 +0300

    Creating user model

--- project_api/models.py
+++ project_api/models.py
@@ -7,4 +7,5 @@ class User(models.Model):
     picture = models.ImageField()
     user_name = models.CharField(max_length=100)
     bio = models.CharField(max_length=1500)
+    email = models.EmailField(max_length=255)
     project= models.FieldFile() 
